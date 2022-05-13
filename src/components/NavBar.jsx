c46233091e4e63dd061d6ccd8ab8b64049874127
DiazLeonel <lolosbarbershop99@gmail.com>
2022-05-13 11:18:27 -0300

    Organizacion

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import '../styles/NavBar.scss';
+
+const NavBar = () => {
+    return(
+        <nav className="navbar">
+            {/* <img src="./public/logo.png"/> */}
+            <ul className="navBar__list">
+                <li className="list__item"><a href="#" className="list__button">INICIO</a></li>
+                <li className="list__item"><a href="#" className="list__button">PRODUCTOS</a></li>
+                <li className="list__item"><a href="#" className="list__button">MARCAS</a></li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default NavBar ;
---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.scss';
+import NavBar from'../components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
