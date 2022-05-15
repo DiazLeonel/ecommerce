a506342971b88d805b678487e0a26bbba6c4800f
DiazLeonel <lolosbarbershop99@gmail.com>
2022-05-15 19:54:50 -0300

    itemCartList

--- src/components/itemCart.js
+++ src/components/itemCart.js
@@ -1,10 +1,12 @@
 import cartLogo from '../img/cart.png'
 import '../styles/itemCart.scss'
+import ItemListContainer from './itemListContainer'
+
 const Carrito = () => {
     return (
         <div className='cartWidget'>
             <img className='itemCart' src={cartLogo} alt="Cart-widget"/>
-            <span className='contador'>0</span>
+            <ItemListContainer />
         </div>
     )
 }
---
+++ src/components/itemListContainer.js
@@ -0,0 +1,7 @@
+const ItemListContainer = () => {
+    return(
+        <span className='contador'>0</span>
+    )
+}
+
+export default ItemListContainer;
