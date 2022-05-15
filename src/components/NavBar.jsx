8760ee73e83ca4dd263f50ab8d9d4a22363328bb
DiazLeonel <lolosbarbershop99@gmail.com>
2022-05-15 17:43:48 -0300

    Menu e-commerce

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,11 @@ import React from "react";
 import '../styles/NavBar.scss';
 
 const NavBar = () => {
-    return(
+    return (
         <nav className="navbar">
+            <div className="title">
+                <h1><a href="#">MusicKS</a></h1>
+            </div>
             {/* <img src="./public/logo.png"/> */}
             <ul className="navBar__list">
                 <li className="list__item"><a href="#" className="list__button">INICIO</a></li>
@@ -16,4 +19,4 @@ const NavBar = () => {
     )
 }
 
-export default NavBar ;
+export default NavBar;
